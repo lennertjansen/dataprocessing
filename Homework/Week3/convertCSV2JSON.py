e018fede8a076e61dd7993f051c3bc27d0866df6
Lennert Jansen <lennertjansen@wcw-stud-145-109-17-220.wireless.uva.nl>
2018-04-23 12:04:29 +0200

    meh

--- Homework/Week3/convertCSV2JSON.py
+++ Homework/Week3/convertCSV2JSON.py
@@ -1,12 +1,18 @@
 # Convert CSV to JSON
 
 import csv
+import pandas
 import json
 
+# open csv file and create and open json file
 csvfile = open('2017.csv', 'r')
 jsonfile = open('2017.json', 'w')
 
+# specify the fieldnames of the columns of interest
 fieldnames = ("Country", "Happiness.Rank", "Happiness.Score", "Economy..GDP.per.Capita.")
+
+# csvfile_clean = pandas.read_csv('2017.csv', names=fieldnames)
+
 reader = csv.DictReader(csvfile, fieldnames)
 for row in reader:
     json.dump(row, jsonfile)
