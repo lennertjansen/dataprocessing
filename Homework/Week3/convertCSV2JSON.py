7da2c024fc97832ab6d784f46b46d95531133bcf
Lennert Jansen <lennertjansen@Lennerts-MBP.fritz.box>
2018-04-28 17:20:58 +0200

    first draft homework

--- Homework/Week3/convertCSV2JSON.py
+++ Homework/Week3/convertCSV2JSON.py
@@ -5,16 +5,14 @@ import pandas
 import json
 
 # open csv file and create and open json file
-csvfile = open('2016_manual.csv', 'r')
-jsonfile = open('2016_manual.json', 'w')
+csvfile = open('2016_top19.csv', 'r')
+jsonfile = open('2016_top19.json', 'w')
 
 # specify the fieldnames of the columns of interest
-#fieldnames = ("Country", "Happiness.Rank", "Happiness.Score", "Economy..GDP.per.Capita.")
 fieldnames = ("Country", "gdp_per_capita")
 
-# csvfile_clean = pandas.read_csv('2017.csv', names=fieldnames)
-
+# read into new json file and write the key value pairs per row
 reader = csv.DictReader(csvfile, fieldnames)
 for row in reader:
     json.dump(row, jsonfile)
-    jsonfile.write('\n')
+    jsonfile.write(',\n')
--- Homework/Week3/script.js
+++ Homework/Week3/script.js
@@ -1,9 +1,18 @@
+/*
+* Name: Lennert Jansen
+* Student number: 10488952
+* Data Processing, Spring 2018
+* Homework assignment week 3
+* Interactive barchart in Javascript
+*/
+
+
 // create two empty lists for JSON data to be stored within said lists
 var countries = [];
 var gdpPerCapita = [];
 
 // retrieve data from JSON file for manipulation with D3
-d3.json("2016_manual_copy.json", function(data) {
+d3.json("2016_top19_correct.json", function(data) {
 
     // store JSON data in seperate lists created above
     for (var iter = 0; iter < data.length; iter++){
@@ -19,89 +28,48 @@ d3.json("2016_manual_copy.json", function(data) {
     var barSpace = 1;
     var padding = 40;
 
-    var formatPercent = d3.format(".0%");
-
-
-    // create SVG element and store it in a variable for future referencing
-    // var svg = d3.select("body")
-    //             .append("svg")
-    //             .attr("class", "chart")
-    //             .attr("width", w  + (padding * 2))
-    //             .attr("height", h + padding);
-
     // generate linear scale for height and create corresponding y axis function
     var yScale = d3.scale.linear()
-                    //.domain([d3.min(gdpPerCapita), d3.max(gdpPerCapita)])
                     .domain([d3.min(gdpPerCapita), d3.max(gdpPerCapita)])
-                    //.range([h - padding, padding]);
                     .range([h - padding, 0]);
     var yAxis = d3.svg.axis()
                     .scale(yScale)
                     .orient("left");
-                    //.ticks(??);
 
     // generate linear scale for height and create corresponding y axis function
     var xScale = d3.scale.ordinal()
                     .domain(countries)
-                    //.rangeRoundBands([0, w], .1);
                     .rangeRoundBands([2, w + padding]);
     var xAxis = d3.svg.axis()
                     .scale(xScale)
                     .orient("bottom");
-                    //.ticks(20);
 
+    // create d3.tip variable and return its label for every bar element
     var tip = d3.tip()
                 .attr('class', 'd3-tip')
                 .offset([-10, 0])
                 .html(function(d) {
                         return "<strong>GDP per capita:</strong> <span style='color:red'>" + d + "</span>";
-                        //return (d);
                 })
 
+    // create svg canvas element with margins and store in variable for later
     var svg = d3.select("body")
                 .append("svg")
                 .attr("class", "chart")
-                .attr("width", w  + (padding * 2) + 40)
-                .attr("height", h + padding + 40);
+                .attr("width", w  + (padding * 3))
+                .attr("height", h + (padding * 2));
 
+    // call tip functionality
     svg.call(tip);
-
-    // // placing a bar for every data value with 'd'
-    // svg.selectAll("rect")
-    //    .data(gdpPerCapita)
-    //    .enter()
-    //    .append("rect")
-    //    .on('mouseover', tip.show)
-    //    .on('mouseout', tip.hide)
-    // // calculating the width of every bar
-    //     .attr("x", function(d, i){
-    //         return (i * ((w + (padding)) / data.length)) + padding
-    //     })
-    //     // and height
-    //     .attr("y", function(d, i){
-    //             return yScale(d)
-    //     })
-    //     // calculating the width of my whole graphic
-    //     .attr("width", w / data.length - barSpace)
-    //     // and its height
-    //     .attr("height", function (d){
-    //         return h - yScale(d);
-    //         })
-    //     // filling the bars with color
-    //     .attr("fill", function(d) {
-    //                 return "rgb(213, 0, " + (d * 10) + ")";
-    //     });
-
-
 
     // generate visual element for the y axis by calling yAxis function
     svg.append("g")
         .attr("class", "y axis")
-        //.attr("transform", "translate(" + 40 + ",40)")
         .attr("transform", "translate(" + padding*0.95 + ",1)")
         .call(yAxis);
 
-    // generate visual element for the x axis by calling xAxis function
+    // generate visual element for the x axis by calling xAxis function and
+    // include rotated labels at every tick for every country
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(40," + (h + barSpace) + ")")
@@ -113,40 +81,29 @@ d3.json("2016_manual_copy.json", function(data) {
         .attr("transform", "rotate(40)")
         .style("text-anchor", "start");
 
-        // placing a bar for every data value with 'd'
+        // create a bar (or rect) for every datum
         svg.selectAll("rect")
            .data(gdpPerCapita)
            .enter()
            .append("rect")
            .attr("class", "chart")
-        // calculating the width of every bar
-            .attr("x", function(d, i){
+           .attr("x", function(d, i){
                 return (i * ((w + (padding)) / data.length)) + padding
-            })
-            // and height
+            }) // calculate x coordinate of rect
             .attr("y", function(d, i){
                     return yScale(d)
-            })
-            // calculating the width of my whole graphic
-            .attr("width", w / data.length - barSpace)
-            // and its height
+            }) // calculate y coordinate of rect
+            .attr("width", w / data.length - barSpace) // determine bar width
             .attr("height", function (d){
                 return h - yScale(d);
-                })
-            .on('mouseover', tip.show)
+            }) // determine bar height
+            .on('mouseover', tip.show) // ensure tip appears and disappears
             .on('mouseout', tip.hide);
-            // filling the bars with color
-            // .attr("fill", function(d) {
-            //             return "rgb(213, 0, " + (d * 10) + ")";
-            // });
 
-    // svg.selectAll(".bar")
-    //     .data(data)
-    //     .enter().append("rect")
-    //     .attr("class", "bar")
-    //     .attr("x", function(d) { return x(d.Country); })
-    //     .attr("width", xScale.rangeBand())
-    //     .attr("y", function(d) { return yScale(d.gdp_per_capita); })
-    //     .attr("height", function(d) { return height - yScale(d.gdp_per_capita); })
-
+    // append text for x axis label
+    var xAxisLabel = svg.append("text")
+                        .attr("x", (w  + (padding * 3)) / 2)
+                        .attr("y", h + (padding * 1.75) )
+                        .style("text-anchor", "middle")
+                        .text("Country");
 });
