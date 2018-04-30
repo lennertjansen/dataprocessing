fa7ea8e810be03152d51fd5a4e917b5ce7da4500
Lennert Jansen <lennertjansen@wcw-stud-145-109-49-26.wireless.uva.nl>
2018-04-30 11:37:44 +0200

    set up finished
    completed first intermediate check point of homework 4

---
+++ Homework/Week4/scatter.js
@@ -0,0 +1,18 @@
+/*
+* Name: Lennert Jansen
+* Student number: 10488952
+* Date: 30 April 2018
+* Scatterplot in JS
+*
+*/
+
+// encapsulate entire script in function
+window.onload = function() {
+
+    // append segment to body containing author and assignment information
+    authorInfo = d3.select("body")
+                .append("p").text("Name: Lennert Jansen")
+                .append("p").text("Student number: 10488952")
+                .append("p").text("Description")
+
+}
